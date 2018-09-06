e1959946671277eae3667dbfe0f56bd03816b788
Alan M.E <alanmarquez@outlook.com>
2018-09-06 08:09:35 -0500

    added BoardDetails
    info general of status play

---
+++ src/main/java/io/amecodelabs/Minesweeper/game/BoardDetails.java
@@ -0,0 +1,37 @@
+package io.amecodelabs.Minesweeper.game;
+
+class BoardDetails {
+	private final int rowsTotal;
+	private final int columnsTotal;
+	private final int num_cells;
+	private final int num_cell_to_Win;
+	private final int mines;
+	
+	BoardDetails(int rowsTotal, int columnsTotal, int num_cells, int mines, int num_cell_to_Win) {
+		this.rowsTotal = rowsTotal;
+		this.columnsTotal = columnsTotal;
+		this.num_cells = num_cells;
+		this.mines = mines;
+		this.num_cell_to_Win = num_cell_to_Win;
+	}
+
+	public int getRowsTotal() {
+		return rowsTotal;
+	}
+
+	public int getColumnsTotal() {
+		return columnsTotal;
+	}
+
+	public int getNum_cells() {
+		return num_cells;
+	}
+
+	public int getNum_cell_to_Win() {
+		return num_cell_to_Win;
+	}
+	
+	public int getMines() {
+		return mines;
+	}
+}
